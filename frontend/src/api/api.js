22253102e4287119c03347f5e248be52bcb5c956
Dhruva Patil <dhruvasp.ca@gmail.com>
2025-06-06 23:45:11 -0300

    feat(backend): Changed the base URL

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -2,8 +2,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  //baseURL: 'http://localhost:3001/api',
-  baseURL: 'https://csci5709-tutorial.netlify.app/'  // matches Express setup
+  baseURL: 'http://localhost:3001/api',
+//   baseURL: 'https://csci5709-tutorial.netlify.app/'  // matches Express setup
 });
 
 export default api;
